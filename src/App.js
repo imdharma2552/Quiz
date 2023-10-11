ac1b7ab158e3c69e007f51d8d38cf8003f64f9ae
Dharma <v.dharma2552@gamil.com>
2023-10-11 15:49:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import Quiz from './components/Quiz'
+import "./App.css"
+function App() {
+  return (
+    <>
+      <Quiz />
+    </>
+  )
+}
+
+export default App
+
+
+// import styles from "./App.css";
+// import Footer from "./component/Footer";
+// import Generator from "./component/Generator";
+// import Navbar from "./component/Navbar";
+
+// function App() {
+//   return (
+//     <div className={styles.container}>
+//       <Navbar />
+//       <Generator />
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import React from 'react'
+// import Generator from './component/Generator'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Generator />
+//     </div>
+//   )
+// }
+
+// export default App
